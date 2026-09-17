d6d9a59967a8719227f32f1fa8b95c64c2569a39
binsquare <hebinsquared@gmail.com>
2026-09-16 20:10:46 -0700

    Link macOS checkpoint tests to the virtualization backend
    Signed-off-by: binsquare <hebinsquared@gmail.com>

--- src/vmm/build.rs
+++ src/vmm/build.rs
@@ -1,7 +1,4 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("macos") {
-        println!("cargo:rustc-link-lib=framework=Hypervisor");
-    }
     emit_snapshot_cfgs();
 }
 
